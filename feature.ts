11d410d6cc0f0864961b47fc1626d0d474b3dd56
mahaalqerem <138065974+mahaalqerem@users.noreply.github.com>
2023-10-25 19:49:08 +0300

    Update feature.ts

--- feature.ts
+++ feature.ts
@@ -1,6 +1,7 @@
 function calculateSum(a: number, b: number): number {
     return a + b + 20;  // Changed to add 20
     // And also add 10
-    //new change.......
+    //new change..by maha.....
     ///////
+    
 }
